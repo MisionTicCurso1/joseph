3bd63c69239f941fe356e7a46ab20394a7806f4d
josephypr <josephyaelromero@gmail.com>
2021-08-13 18:55:07 -0500

    joseph

---
+++ codigo/joseph.py
@@ -0,0 +1,28 @@
+print("hola")
+a=1 
+b=2
+c=3 
+d=4 
+e=5 
+print("buenas tardes este es el catalogo de helados")
+print("MAX 2 HELADOS")
+print("1 el helado sin topping cuesta 1.900")
+print("2 el helado de oreo cuesta 1.000")
+print("3 el helado de KitKat cuesta 1.500")
+print("4 el helado de brownie cuesta 2.500")
+print("5 el helado de barquillo cuesta 950")
+print("que numero de helado desea?")
+#entrada de datos de la consola 
+q= int (input()) 
+w= int (input())
+print("vale estamos procesando el valor")
+g= 1+2+3+4+5
+print ("resultado",a,b,c,d,e)
+
+
+
+
+
+
+
+
